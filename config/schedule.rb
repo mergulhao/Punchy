dd4c694dedf73dde602834f22431d13408ceaf35
Sylvestre Mergulhão <contato@mergulhao.info>
2011-07-23 11:42:47 -0300

    add schedule.rb

---
+++ config/schedule.rb
@@ -0,0 +1,30 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+env :PATH, "/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games"
+env :RAILS_ENV, "production"
+job_type :command, "cd :path && :task :output"
+set :job_template, nil
+set :output, "/home/railsapps/punchy/shared/log/whenever.log"
+
+every 1.minute do
+  runner "Punch.create(:body => Time.now.to_s(:db))"
+end
